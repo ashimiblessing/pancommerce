ce297e988e1e0d488bad148efdd366cf2f926179
Blessing Ashimi <ashimiblessing@hotmail.com>
2021-08-08 14:30:24 +0100

    just added header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+import brand from './lumin.png';
+import cart from './cart.png';
 import './App.css';
 
 function App() {
   return (
+
+ 
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header>
+
+
+
+
+
+
+     <div className="nav">
+     <img src={brand} className="brand"/> 
+<ul>
+             <li><a href="#">Shop</a></li>
+             <li><a href="#">About</a></li>
+             <li><a href="#">Support</a></li>
+             <li><a href="#">Blog</a></li>
+             
+         </ul>
+ 
+
+     </div>
+
+     <div className="right-nav">
+
+
+<a>Account</a> <a ><img src={cart} className="cart-img" /></a>
+
+
+
+
+</div>
+
+
+ </header>
+
+
+
+
+<div className="sub-head">
+  
+</div>
+
+
+
     </div>
+
+
+ 
   );
 }
 
